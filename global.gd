c0d56cf6dbad296ed4282e946834bc67490e7c40
jtlung <jtlung01@gmail.com>
2023-09-16 23:26:38 -0400

    completed part 2

--- global.gd
+++ global.gd
@@ -1,6 +1,9 @@
 extends Node
 
+func _ready():
+	randomize()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("Quit"):
 		get_tree().quit()
+r
